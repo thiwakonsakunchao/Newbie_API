3e1e3137a3bc063fdf3490bfc8996ab16703f25f
thiwakonsakunchao <thiwakon_sakunchao@cmu.ac.th>
2024-11-18 18:37:39 +0700

    Add and Delete v1

--- src/Controller/userController.ts
+++ src/Controller/userController.ts
@@ -24,3 +24,44 @@ export const addUser = async ( req: Request, res: Response ): Promise<void> => {
         
     }
 }
+
+export const updateUser = async ( req: Request, res: Response ): Promise<void> => {
+    try {
+
+        const { _id: id } = req.params;
+        const { username, email, age } = req.body;
+        const user = { username, email, age };
+        const data = await User.find()
+        
+        const result = await User.findByIdAndUpdate(id, user, { new: true });
+
+
+        res.status(200).json({
+            result
+        });
+        
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+export const deleteUser = async ( req: Request, res: Response ): Promise<void> => {
+    try {
+        
+        console.log(req.params);
+        
+        const { _id: id } = req.params;
+
+        
+        const result = await User.findByIdAndDelete(id, { new: true });
+
+        res.status(200).json({
+            result
+        });
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
--- src/Router/userRouter.ts
+++ src/Router/userRouter.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { getUser, addUser  } from '../Controller/userController';
+import { getUser, addUser, updateUser, deleteUser  } from '../Controller/userController';
 
 const router = Router();
 
-router.get("/get", getUser);
-router.post("/add", addUser);
+router.get('/get', getUser);
+router.post('/add', addUser);
+router.put('/update/:_id', updateUser)
+router.delete('/delete/:id', deleteUser)
 
 export default router;
 
